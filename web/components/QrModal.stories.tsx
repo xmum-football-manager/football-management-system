9b97177e174c94bfa5632b5cfa09d66992675b9a
josh lee <joshleethecreator@gmail.com>
2026-05-12 01:37:45 +0800

    fix: correct storybook/test import path in stories

--- web/components/QrModal.stories.tsx
+++ web/components/QrModal.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/nextjs-vite'
-import { fn } from '@storybook/test'
+import { fn } from 'storybook/test'
 import { QrModal } from './QrModal'
 
 const meta: Meta<typeof QrModal> = {
